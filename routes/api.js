da8a570f2b17c99f9e59c371984f85b9ce79e30a
Glitch (s-projects18-fcc-62) <none>
2019-09-22 08:24:06 +0000

    added deleteIssue; better error handling for frontend by adding status(500)

--- routes/api.js
+++ routes/api.js
@@ -8,15 +8,7 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
 var database = require('../helper/database.js');
-
-/*const CONNECTION_STRING = process.env.DB;
-MongoClient.connect(CONNECTION_STRING, function(err, db) {
-  console.log("connected");
-});*/
 
 module.exports = function (app) {
 
@@ -25,19 +17,18 @@ module.exports = function (app) {
     // I can filter my get request by also passing along any field and value in the query
     .get(function (req, res){
       const projectname = req.params.project;
-      // TODO: maybe some checks; allowed field ?
+// TODO: maybe some checks; allowed field ?
       const filter = req.query;
       const project = database.getProject(projectname, (err, doc)=>{
-        if(err==null) {
-          doc.error=false;
-          res.json(doc);
-        } else {
+        if(err==null) res.json(doc);
+        else {
           console.log(err);
-          res.json({error:true, msg:err});
+          res.status(500).send(err.message);
         }
       }, filter);
     })
     
+// TODO: check mandatory fields and send 500
     // I can POST /api/issues/{projectname} with form data containing
     // required issue_title, issue_text, created_by, and optional assigned_to and status_text
     .post(function (req, res){
@@ -47,20 +38,39 @@ module.exports = function (app) {
       database.insertIssue(req.body, (err, doc) => {
         if(err==null) res.json(doc);
         else {
-          err.error=true;
-          res.json(err);
+          console.log(err);
+          res.status(500).send(err.message);
         }
       });
     })
     
+    // I can PUT /api/issues/{projectname} with a _id and any fields in the object with a value to object said object
     .put(function (req, res){
-      var project = req.params.project;
-      
+      //cannot be updated - at least not here (danger of inconsistent issues)
+      //var projectname = req.params.project;
+    
+      database.updateIssue(req.body._id, req.body, (err, doc) => {
+        if(err==null) res.json(doc);
+        else {
+          console.log(err);
+          res.status(500).send(err.message);
+        }
+      });
     })
     
+    // I can DELETE /api/issues/{projectname} with a _id to completely delete an issue. 
+    // If no _id is sent return '_id error', success: 'deleted '+_id, failed: 'could not delete '+_id.
     .delete(function (req, res){
-      var project = req.params.project;
-      
+      var projectname = req.params.project;
+      database.deleteIssue(req.body._id, (err, resultObject) => {
+        if(err==null) {
+          if(resultObject.deletedCount==0) res.json({failed: 'could not delete ' + req.body._id});
+          else res.json({success: 'deleted ' + req.body._id});
+        } else {
+          console.log(err);
+          res.status(500).send(err.message);
+        }
+      });
     });
     
 };
